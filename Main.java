0ad17886bf90e5c806366772d1b7db38c915038a
Christian Lombardo <christianlombardo@verizon.net>
2021-10-18 16:47:21 -0400

    Commit more code

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Revature! Exciting!");
+        System.out.println("Hello Revature! Exciting! DOING IT!");
+        System.out.println("Be Easy. Have Fun. Learn Grow Expand.");
     }
 }
